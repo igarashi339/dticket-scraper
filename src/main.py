c8135a286c5aa393e56665d48d8db06631bc8642
igarashi339 <igarashi339@gmail.com>
2021-12-13 21:13:45 +0000

    importまわりの記述を修正

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import os
 import time
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from tweet_handler import TweetHandler
 from selenium import webdriver
 from line_handler import LineHandler
@@ -32,7 +32,7 @@ def exec_single_month(driver, line_handler, tweet_handler):
             # 1デーパスポート
             now_on_sale_str = "//*[@id=\"searchResultList\"]/ul/li[1]/div/input"
             now_on_sale_elem = driver.find_element_by_xpath(now_on_sale_str)
-            dt_now_utc_aware = datetime.datetime.now(datetime.timezone.utc)
+            dt_now_utc_aware = datetime.now(timezone(timedelta(hours=9)))
             tweet_handler.post_tweet(f"{month_str}月{target_date}日の1デーパスポートが空いてるよ！\n{URL}\n\n"
                                      f"※{dt_now_utc_aware.strftime('%Y/%m/%d/ %H:%M:%S')}時点の情報です")
             time.sleep(1)
