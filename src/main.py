fc236b15d8504fa5b0b23901b55db70dd77707f5
igarashi339 <igarashi339@gmail.com>
2022-02-20 17:32:15 +0000

    クローリング手法を変更

--- src/main.py
+++ src/main.py
@@ -1,112 +1,120 @@
 import os
 import time
 from datetime import datetime, timezone, timedelta
+from dateutil.relativedelta import relativedelta
 from tweet_handler import TweetHandler
 from selenium import webdriver
 from line_handler import LineHandler
 from db_handler import DBHandler
+import urllib
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-URL = os.environ["SCRAPING_TARGET_URL"]
+TARGET_URL = os.environ["SCRAPING_TARGET_URL"]
 WEEKDAY_LIST = ["月","火","水","木","金","土","日"]
-TARGET_MONTH_NUM = 3  # 当月含めて何か月分チェックするか
+EXEC_PER_HOUR = 2 # 30分に何回転させるか
 
 
-def exec_single_month(driver, line_handler, tweet_handler, db_hanlder):
-    year_str = driver.find_element_by_xpath("//*[@id=\"searchCalendar\"]/div/h3/span[1]").text.strip("年")
-    month_str = driver.find_element_by_xpath("//*[@id=\"searchCalendar\"]/div/h3/span[2]").text.strip("月")
-    dt_global = None
-    for target_date in range(1, 32):
-        try:
-            date_elem_list = driver.find_elements_by_class_name("date")
-            target_date_str = str(target_date)
-            for date_elem in date_elem_list:
-                if date_elem.text == target_date_str:
-                    target_date_elem = date_elem
-                    break
-            dt = datetime(year=int(year_str), month=int(month_str), day=int(target_date),tzinfo=timezone(timedelta(hours=+9)))
-            dt_now = datetime.now(timezone(timedelta(hours=9)))
-            if dt_now > dt:
-                continue
-            dt_global = dt
-            weekday_str = WEEKDAY_LIST[dt.weekday()]
-            print(f"{year_str}/{month_str}/{target_date}({weekday_str})チェック...")
-            target_date_elem.click()
-            time.sleep(1)
-            # view on smart phone
-            view_on_sp_str = "//*[@id=\"searchEticket\"]"
-            view_on_sp_elem = driver.find_element_by_xpath(view_on_sp_str)
-            view_on_sp_elem.click()
-            time.sleep(15)
-            # 1デーパスポート
-            now_on_sale_str = "//*[@id=\"searchResultList\"]/ul/li[1]/div/input"
-            driver.find_element_by_xpath(now_on_sale_str)
-            driver.find_element_by_xpath("//*[@id=\"searchResultList\"]/ul/li[1]/div").click()
-            time.sleep(15)
-            # TDS, TDL
-            tdl_str = driver.find_element_by_xpath("//*[@id=\"search-ticket-group\"]/div/section/div[2]/section[1]/div[1]/div/ul/li[1]/span").text
-            tds_str = driver.find_element_by_xpath("//*[@id=\"search-ticket-group\"]/div/section/div[2]/section[1]/div[1]/div/ul/li[2]/span").text
-            tdl_is_available = ("販売していません" not in tdl_str)
-            tds_is_available = ("販売していません" not in tds_str)
-            tdl_available_str = "〇" if tdl_is_available else "×"
-            tds_available_str = "〇" if tds_is_available else "×"
-            # 元の画面に戻る
-            driver.find_element_by_xpath("//*[@id=\"search-ticket-group\"]/div/section/div[1]/div/a/i").click()
+def get_target_date_obj_list():
+    """
+    クローリング対象のdatetimeオブジェクトのリストを取得する。
+    本日から2か月間を対象とする。
+    """
+    current_date = datetime.now(timezone(timedelta(hours=9)))
+    end_date = current_date + relativedelta(months=2) - timedelta(days=2)
+    target_date_list = [current_date]
+    should_continue = True
+    while should_continue:
+        next_date = current_date + timedelta(days=1)
+        current_date = next_date
+        target_date_list.append(next_date)
+        if next_date > end_date:
+            should_continue = False
+    return target_date_list
 
-            # ツイートするか決定
-            should_tweet = False
-            if weekday_str == "土" or weekday_str == "日":
-                prev_land_available = db_hanlder.select_from_dticket_status(dt, "land")
-                prev_sea_available = db_hanlder.select_from_dticket_status(dt, "sea")
-                should_tweet = (not prev_land_available and tdl_is_available) or (not prev_sea_available and tds_is_available)
 
-            # DB更新
-            db_hanlder.update_dticket_status_record(dt, "land", tdl_is_available)
-            db_hanlder.update_dticket_status_record(dt, "sea", tds_is_available)
+def fetch_single_date_ticket_info(driver, target_date_obj):
+    target_date_str = target_date_obj.strftime('%Y%m%d')
+    param_dict = {
+        "parkTicketGroupCd": "020",
+        "numOfAdult": "2",
+        "numOfJunior": "0",
+        "numOfChild": "0",
+        "parkTicketSalesForm": "1",
+        "useDays": "1",
+        "route": "1",
+        "selectParkDay1": "01",
+        "useDateFrom": target_date_str
+    }
+    param = urllib.parse.urlencode(param_dict)
+    driver.get(TARGET_URL + "?" + param)
+    time.sleep(5)
+    tdl_str = driver.find_element_by_xpath("//*[@id=\"search-ticket-group\"]/div/section/div[2]/section[1]/div[1]/div/ul/li[1]/span").text
+    tds_str = driver.find_element_by_xpath("//*[@id=\"search-ticket-group\"]/div/section/div[2]/section[1]/div[1]/div/ul/li[2]/span").text
+    tdl_is_available = ("販売していません" not in tdl_str)
+    tds_is_available = ("販売していません" not in tds_str)
+    return tdl_is_available, tds_is_available
 
-            # Post Tweet
-            if should_tweet:
-                dt_now_utc_aware = datetime.now(timezone(timedelta(hours=9)))
-                # todo: landとsea両方とも協調できるようになったらそうする
-                param_type = "land" if tdl_is_available else "sea"
-                param_date = year_str + "-" + month_str.zfill(2) + "-" + target_date_str.zfill(2)
-                tweet_handler.post_tweet(f"{year_str}/{month_str}/{target_date}({weekday_str})の1デーパス空いてるよ！\n"
-                                         f"ランド{tdl_available_str} シー{tds_available_str}\n"
-                                         f"https://tdr-plan.com/ticket?type={param_type}&date={param_date}\n"
-                                         f"※{dt_now_utc_aware.strftime('%Y/%m/%d %H:%M:%S')}時点の情報\n"
-                                         f"#ディズニー #ディズニーチケット")
-            time.sleep(1)
-        except Exception as e:
-            # DB更新
-            if dt_global:
-                db_hanlder.update_dticket_status_record(dt_global, "land", False)
-                db_hanlder.update_dticket_status_record(dt_global, "sea", False)
-            print(e)
-            time.sleep(1)
-    try:
-        # > が存在する場合、それをクリックしてreturn
-        next_month_button = driver.find_element_by_xpath("//*[@id=\"searchCalendar\"]/div/div/ul/button[2]")
-        next_month_button.click()
-    except Exception as e:
-        print(e)
+
+def get_should_tweet(db_handler, target_datetime_obj, tdl_is_available, tds_is_available):
+    """
+    スクレイピング結果を受けてTweetするかを決定する。
+    """
+    weekday_str = WEEKDAY_LIST[target_datetime_obj.weekday()]
+    if weekday_str == "土" or weekday_str == "日":
+        prev_land_available = db_handler.select_from_dticket_status(target_datetime_obj, "land")
+        prev_sea_available = db_handler.select_from_dticket_status(target_datetime_obj, "sea")
+        return (not prev_land_available and tdl_is_available) or (not prev_sea_available and tds_is_available)
+    return False
+
+
+def post_tweet(tweet_handler, target_date_obj, tdl_is_available, tds_is_available):
+    dt_now_utc_aware = datetime.now(timezone(timedelta(hours=9)))
+    weekday_str = WEEKDAY_LIST[target_date_obj.weekday()]
+    tdl_available_str = "〇" if tdl_is_available else "×"
+    tds_available_str = "〇" if tds_is_available else "×"
+    # todo: landとsea両方とも協調できるようになったらそうする
+    param_type = "land" if tdl_is_available else "sea"
+    param_date = format(target_date_obj, '%Y-%m-%d')
+    tweet_handler.post_tweet(f"{format(target_date_obj, '%Y/%m/%d')}({weekday_str})の1デーパス空いてるよ！\n"
+                             f"ランド{tdl_available_str} シー{tds_available_str}\n"
+                             f"https://tdr-plan.com/ticket?type={param_type}&date={param_date}\n"
+                             f"※{dt_now_utc_aware.strftime('%Y/%m/%d %H:%M:%S')}時点の情報\n"
+                             f"#ディズニー #ディズニーチケット")
 
 
 def main():
     driver = webdriver.Remote(
-        command_executor=os.environ["SELENIUM_URL"],
-        desired_capabilities=DesiredCapabilities.FIREFOX.copy())
+            command_executor=os.environ["SELENIUM_URL"],
+            desired_capabilities=DesiredCapabilities.FIREFOX.copy())
     driver.implicitly_wait(5)
-    driver.get(URL)
-    line_handler = LineHandler()
+    # line_handler = LineHandler()
     tweet_handler = TweetHandler()
     db_handler = DBHandler()
-    for i in range(TARGET_MONTH_NUM):
-        exec_single_month(driver, line_handler, tweet_handler, db_handler)
-        time.sleep(5)
+    target_date_obj_list = get_target_date_obj_list()
+    for i in range(EXEC_PER_HOUR):
+        for target_datetime_obj in target_date_obj_list:
+            target_datetime_str = format(target_datetime_obj, '%Y/%m/%d')
+            try:
+                tdl_is_available, tds_is_available = fetch_single_date_ticket_info(driver, target_datetime_obj)
+            except Exception as e:
+                print(f"クローリングに失敗しました: {target_datetime_str}")
+                print(e)
+                continue
+            print(target_datetime_str, tdl_is_available, tds_is_available)
+            should_tweet = get_should_tweet(db_handler, target_datetime_obj, tdl_is_available, tds_is_available)
+            db_handler.update_dticket_status_record(target_datetime_obj, "land", tdl_is_available)
+            db_handler.update_dticket_status_record(target_datetime_obj, "sea", tds_is_available)
+            if should_tweet:
+                post_tweet(tweet_handler, target_datetime_obj, tdl_is_available, tds_is_available)
+            time.sleep(5)
     driver.quit()
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
 
