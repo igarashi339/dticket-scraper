82d0fd84d790c74d0868032af8e35ce533de40d2
igarashi339 <igarashi339@gmail.com>
2022-02-21 20:43:07 +0900

    リトライ回数を調整

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 TARGET_URL = os.environ["SCRAPING_TARGET_URL"]
 WEEKDAY_LIST = ["月","火","水","木","金","土","日"]
 EXEC_PER_HOUR = 2 # 何回転させるか
-RETRY_NUM = 4
+RETRY_NUM = 5
 
 
 def get_target_date_obj_list():
